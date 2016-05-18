c4cee3f15559832df01d4889bf94a2c4c4d95c66
Christopher Starcher <cstarcher@users.noreply.github.com>
2016-05-17 20:19:22 -0500

    Add files via upload
    Version 0.7.2

---
+++ getset.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+"""Placeholder."""
+
+
+class GetSet():
+    """Placeholder."""
+
+    def __init__(self):
+        """Placeholder."""
+        self.csv_path = None
+        self.bit_path = None
+        self.archive_path = None
+        self.create_zip = None
+        self.split_zip = None
+        self.zip_size = 0
+        self.zip_unit = None
+        self.create_license = None
+        self.license_file = None
+        self.license_bundle = None
+        self.license_text = None
+        self.restrict_access = None
+        self.group_name = None
+
+    @property
+    def get_csv_path(self):
+        """Placeholder."""
+        return self.csv_path
+
+    @get_csv_path.setter
+    def set_csv_path(self, csv_path):
+        """Placeholder."""
+        self.csv_path = csv_path
+
+    @property
+    def get_bit_path(self):
+        """Placeholder."""
+        return self.bit_path
+
+    @get_bit_path.setter
+    def set_bit_path(self, bit_path):
+        """Placeholder."""
+        self.bit_path = bit_path
+
+    @property
+    def get_archive_path(self):
+        """Placeholder."""
+        return self.archive_path
+
+    @get_archive_path.setter
+    def set_archive_path(self, archive_path):
+        """Placeholder."""
+        self.archive_path = archive_path
+
+    @property
+    def get_create_zip(self):
+        """Placeholder."""
+        return self.create_zip
+
+    @get_create_zip.setter
+    def set_create_zip(self, create_zip):
+        """Placeholder."""
+        self.create_zip = create_zip
+
+    @property
+    def get_split_zip(self):
+        """Placeholder."""
+        return self.split_zip
+
+    @get_split_zip.setter
+    def set_split_zip(self, split_zip):
+        """Placeholder."""
+        self.split_zip = split_zip
+
+    @property
+    def get_zip_size(self):
+        """Placeholder."""
+        return self.zip_size
+
+    @get_zip_size.setter
+    def set_zip_size(self, zip_size):
+        """Placeholder."""
+        self.zip_size = zip_size
+
+    @property
+    def get_zip_unit(self):
+        """Placeholder."""
+        return self.zip_unit
+
+    @get_zip_unit.setter
+    def set_zip_unit(self, zip_unit):
+        """Placeholder."""
+        self.zip_size = zip_unit
+
+    @property
+    def get_create_license(self):
+        """Placeholder."""
+        return self.create_license
+
+    @get_create_license.setter
+    def set_create_license(self, create_license):
+        """Placeholder."""
+        self.create_license = create_license
+
+    @property
+    def get_license_file(self):
+        """Placeholder."""
+        return self.license_file
+
+    @get_license_file.setter
+    def set_license_file(self, license_file):
+        """Placeholder."""
+        self.license_file = license_file
+
+    @property
+    def get_license_bundle(self):
+        """Placeholder."""
+        return self.license_bundle
+
+    @get_license_bundle.setter
+    def set_license_bundle(self, license_bundle):
+        """Placeholder."""
+        self.license_bundle = license_bundle
+
+    @property
+    def get_license_text(self):
+        """Placeholder."""
+        return self.license_text
+
+    @get_license_text.setter
+    def set_license_text(self, license_text):
+        """Placeholder."""
+        self.license_text = license_text
+
+    @property
+    def get_restrict_access(self):
+        """Placeholder."""
+        return self.restrict_access
+
+    @get_restrict_access.setter
+    def set_restrict_access(self, restrict_access):
+        """Placeholder."""
+        self.restrict_access = restrict_access
+
+    @property
+    def get_group_name(self):
+        """Placeholder."""
+        return self.group_name
+
+    @get_group_name.setter
+    def set_group_name(self, group_name):
+        """Placeholder."""
+        self.group_name = group_name
---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Placeholder."""
+
+import tkinter as tk
+import gui
+import guiwin
+import getset
+import platform
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    gs = getset.GetSet()
+    if platform.system() == 'Windows':
+        guiwin.GuiTk(root, gs)
+    else:
+        gui.GuiTk(root, gs)
+    root.mainloop()
